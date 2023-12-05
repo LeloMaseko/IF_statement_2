f77233b744233535ce3b00e69608b553683ee6be
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 13:58:13 +0200

    Add project files.

---
+++ IF_statement_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_statement_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getMax(2, 10));
+
+            int getMax(int num1, int num2)
+            {
+                if (num1 > num2)
+                {
+                    return num1;
+                }
+                else
+                {
+                    return num2;
+                }
+            }
+
+        }
+    }
+}
